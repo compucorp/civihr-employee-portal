5544699e93e21f8e2fc093dc324b58d2fd65e05d
gregmeszaros <meszaros275@gmail.com>
2014-11-13 16:35:35 +0000

    Sprint 1 changes

--- civihr_employee_portal/js/filters.js
+++ civihr_employee_portal/js/filters.js
@@ -2,6 +2,76 @@
 Drupal.behaviors.civihr_employee_portal_filters = {
     attach: function (context, settings) {
         
+        // Load only when the context is front page (we don't want to load this script in modal windows or when requesting leave)
+        if (context == document) {
+        
+            if ($('table').hasClass('manager-approval-main-table') && $('div').hasClass('ctools-modal-dialog') == false) {
+
+                // Header table sorter
+                $(".manager-approval-main-table").tablesorter();
+
+            }
+        
+        }
+        
+        var ApprovalFilter = function () {
+            
+            var $table = $("table.manager-approval-main-table");
+            
+            // Float the headers
+            $table.floatThead({
+                    
+                scrollContainer: function($table){
+                    return $table.closest('.approval-overflow-content');
+                },
+                useAbsolutePositioning: true
+            });
+            
+        };
+
+        ApprovalFilter.prototype.removeHighlighting = function(highlightedElements) {
+            highlightedElements.each(function(){
+                var element = $(this);
+                
+                element.replaceWith(element.html());
+            })
+        };
+        
+        ApprovalFilter.prototype.addHighlighting = function(element, textToHighlight) {
+            var text = element.text();
+            var highlightedText = '<em>' + textToHighlight + '</em>';
+            var newText = text.replace(textToHighlight, highlightedText);
+            
+            element.html(newText);
+        };
+        
+        // Init the approval highlight search
+        var approvalsearch = new ApprovalFilter();
+
+        $("#manager-approval-search").on("keyup", function() {
+            var value = $(this).val();
+
+            approvalsearch.removeHighlighting($(".manager-approval-main-table > tbody tr em"));
+
+            $(".manager-approval-main-table > tbody tr").each(function(index) {
+                $row = $(this);
+
+                // Find the Contact name element
+                var $tdElement = $row.find(".views-field-civi-target-contact-name");
+
+                var id = $tdElement.text().trim();
+                var matchedIndex = id.indexOf(value);
+
+                if (matchedIndex != 0) {
+                    $row.hide();
+                }
+                else {
+                    approvalsearch.addHighlighting($tdElement, value);
+                    $row.show();
+                }
+            });
+        });
+        
         if ($('div').hasClass('approval-filters') && $('div').hasClass('ctools-modal-dialog') == false) {
             
             // Function to create a distinct list from array
@@ -40,7 +110,7 @@ Drupal.behaviors.civihr_employee_portal_filters = {
             tagItem = '<button class="btn btn-custom" type="button" class="active">all&nbsp;<span class="badge">' + classList['approvals-table'] + '</span></button>';
 
             // Check for the enabled absence types only
-            var allowed_values = ["Vacation", "Maternity", "TOIL"];
+            var allowed_values = ["Vacation", "Maternity", "TOIL", "Approved", "Rejected", "Cancelled", "Awaiting approval"];
             
             // Loop through the list of classes & add link
             $.each(classList, function(index, value) {
