81a16e3bdd5534a81feb77a502c69f757fe174fb
gregmeszaros <meszaros275@gmail.com>
2015-02-19 13:56:41 +0000

    Templates cleanup, work on removing the dependency on radix theme 50%

---
+++ civihr_employee_portal/views/templates/views-view--approvals--block-1.tpl.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Main view template.
+ *
+ * Variables available:
+ * - $classes_array: An array of classes determined in
+ *   template_preprocess_views_view(). Default classes are:
+ *     .view
+ *     .view-[css_name]
+ *     .view-id-[view_name]
+ *     .view-display-id-[display_name]
+ *     .view-dom-id-[dom_id]
+ * - $classes: A string version of $classes_array for use in the class attribute
+ * - $css_name: A css-safe version of the view name.
+ * - $css_class: The user-specified classes names, if any
+ * - $header: The view header
+ * - $footer: The view footer
+ * - $rows: The results of the view query, if any
+ * - $empty: The empty text to display if the view is empty
+ * - $pager: The pager next/prev links to display, if any
+ * - $exposed: Exposed widget form/info to display
+ * - $feed_icon: Feed icon to display, if any
+ * - $more: A link to view more, if any
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <?php print $title; ?>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php if ($header): ?>
+    <div class="view-header">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($exposed): ?>
+    <div class="view-filters">
+      <?php print $exposed; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($attachment_before): ?>
+    <div class="attachment attachment-before">
+      <?php print $attachment_before; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($rows): ?>
+    
+    <div class="approval-filters col-sm-2-no-padding column1 panel-panel">
+        
+    </div>
+    
+    <div class="view-content col-md-10-no-padding column2 panel-panel">
+        <div class="well-small">
+            
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="btn input-group-addon"><img src="http://findicons.com/files/icons/1389/g5_system/32/toolbar_find.png" height="16px" width="16px" class="img-rounded" /></div>
+                    <input class="form-control" id="manager-approval-search" placeholder="Enter name">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    <div class="view-content col-md-10-no-padding column2 panel-panel">
+        
+        <div class ="approval-overflow-content" style="overflow: scroll; height: 250px;">
+            <?php print $rows; ?>
+        </div>
+      
+    </div>
+  <?php elseif ($empty): ?>
+    <div class="view-empty">
+      <?php print $empty; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($pager): ?>
+    <?php print $pager; ?>
+  <?php endif; ?>
+
+  <?php if ($attachment_after): ?>
+    <div class="attachment attachment-after">
+      <?php print $attachment_after; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($more): ?>
+    <?php print $more; ?>
+  <?php endif; ?>
+
+  <?php if ($footer): ?>
+    <div class="view-footer">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($feed_icon): ?>
+    <div class="feed-icon">
+      <?php print $feed_icon; ?>
+    </div>
+  <?php endif; ?>
+
+</div><?php /* class view */ ?>
