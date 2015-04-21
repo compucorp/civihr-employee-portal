592c6bccabf31816b377ba1c926ed93e4137b7af
gregmeszaros <meszaros275@gmail.com>
2015-04-21 17:19:54 -0600

    Fixes

--- civihr_employee_portal/src/Blocks/SicknessLinks.php
+++ civihr_employee_portal/src/Blocks/SicknessLinks.php
@@ -10,27 +10,9 @@ class SicknessLinks {
      */
     public function generateBlock() {
 
-        /*
-        ctools_include('modal');
-        ctools_modal_add_js();
-
-        // Create our own javascript that will be used to theme a modal.
-        $civihr_style = array(
-            'civihr-default-style' => array(
-                'modalOptions' => array(
-                    'opacity' => .5,
-                    'background-color' => '#000',
-                ),
-                'animation' => 'fadeIn',
-            ),
-        );
-
-        drupal_add_js($civihr_style, 'setting');
-        */
-
         $links = '';
         $links .= '<div id="absence-links" class="list-group" style="height: 50px;">';
-        $links .= civihr_employee_portal_make_link(t('Report new nickness'), 'sick');
+        $links .= civihr_employee_portal_make_link(t('Report new sickness'), 'sick');
 
 
         $links .= '</div>';
