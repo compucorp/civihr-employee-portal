965bd795277dc8776f7b63ee49f6a8a5b8dda88b
Gergely Meszaros <greg@compucorp.co.uk>
2015-05-06 17:26:46 +0100

    Print leave report dev

---
+++ civihr_employee_portal/templates/civihr-employee-portal-print-leave-report.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gergelymeszaros
+ * Date: 06/05/15
+ * Time: 16:44
+ */
+
+?>
+
+
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($custom_data); ?>
+
+</div>
