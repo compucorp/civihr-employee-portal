fb812cbc6570ae281caa1bea1aad0236935f760c
Gergely Meszaros <greg@compucorp.co.uk>
2015-06-08 16:43:47 +0100

    Leave approval rules

--- civihr_employee_portal/src/Helpers/HelperClass.php
+++ civihr_employee_portal/src/Helpers/HelperClass.php
@@ -36,5 +36,48 @@ class HelperClass {
 
         return $result;
     }
+
+    /**
+     * @param $contact_id
+     * @return mixed
+     * Pass the contact ID and this function will return array of assigned manager contact IDs
+     */
+    public static function _get_contact_manager_contact_id($contact_id) {
+
+        // Civi init
+        civicrm_initialize();
+
+        $res = civicrm_api3('Relationship', 'get', array('contact_id' => $contact_id));
+        $contactRelationships = $res['values'];
+
+        $assigned_manager_contact_ids = [];
+        $manager_found = 0;
+
+        // If Leave approver is find, assign him as the manager (add contact ID to $assigned_manager_contact_ids array)
+        foreach ($contactRelationships as $key => $relation) {
+            if ($relation['relation'] == 'has Leave Approved by') {
+                $assigned_manager_contact_ids[] = $relation['contact_id_b'];
+                $manager_found++;
+            }
+        }
+
+        // If no assigned managers found
+        if ($manager_found <= 0) {
+
+            // Get the main admin contact (this will be the default approver -> as no other leave approver is found)
+            $main_admin_contact = civicrm_api('uf_match', 'get', array(
+                'version' => 3,
+                'uf_id' => 1,
+            ));
+
+            $main_admin_contact = array_shift($main_admin_contact['values']);
+
+            // Set default manager ID
+            $assigned_manager_contact_ids[] = $main_admin_contact['contact_id'];
+
+        }
+
+        return $assigned_manager_contact_ids;
+    }
     
 }
