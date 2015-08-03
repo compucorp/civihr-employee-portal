d322f5bfbfdd3efaf1585794ae1b7ceb7438d394
Gergely Meszaros <greg@compucorp.co.uk>
2015-08-03 14:18:06 +0100

    Custom report template page

---
+++ civihr_employee_portal/templates/civihr-employee-portal-custom-reports.tpl.php
@@ -0,0 +1,11 @@
+<div class="col-md-2 column1 panel-panel">
+    <button id="headcount" class="btn btn-primary btn-reports">Headcount</button>
+    <button id="gender" class="btn btn-primary btn-reports">Gender</button>
+    <button id="age" class="btn btn-primary btn-reports">Age</button>
+</div>
+
+template
+<div class="col-md-10 column2 panel-panel">
+    <div id="custom-report"></div>
+    <div id="custom-report-details"> <?php print $custom_data; ?></div>
+</div>
