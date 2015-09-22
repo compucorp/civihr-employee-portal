e00bbfe7ccb08e3981584e725ec64a269fb6f19a
Gergely Meszaros <greg@compucorp.co.uk>
2015-09-22 15:57:33 +0100

    PCHR-451 age groups cleanup part 2

--- civihr_employee_portal/src/Forms/BaseForm.php
+++ civihr_employee_portal/src/Forms/BaseForm.php
@@ -35,7 +35,11 @@ trait modalCallback {
                 ctools_add_js('ajax-responder');
 
                 $output[] = ctools_modal_command_dismiss();
-                $output[] = ctools_ajax_command_redirect($redirect);
+                $output[] = ajax_command_remove('#messages');
+                $output[] = ajax_command_after('#breadcrumb', '<div id="messages">' . theme('status_messages') . '</div>');
+
+                // If we need to redirect
+                //$output[] = ctools_ajax_command_redirect($redirect);
 
             }
 
