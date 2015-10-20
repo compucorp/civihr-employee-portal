5c3182bfa7990c62868837afb4f8fa2e4088b4e5
Alessandro Verdura <alessandro@compucorp.co.uk>
2015-10-20 12:24:08 +0200

    PCHR-515: Draft of chr_section implementation

--- civihr_employee_portal/templates/civihr-employee-portal-custom-reports.tpl.php
+++ civihr_employee_portal/templates/civihr-employee-portal-custom-reports.tpl.php
@@ -1,41 +1,42 @@
 <?php $enabled_y_axis_filters = variable_get('enabled_y_axis_filters', array()); ?>
 
-<div class="panel-pane pane-block">
+<section class="chr_section">
+    <header class="chr_section__header">
+        <h2 class="chr_section__title">
+            My People
+        </h2>
+    </header>
+    <div class="chr_section__body">
+        <div class="row">
+            <div class="col-md-2 column1 panel-panel">
+                <?php
+                    foreach ($enabled_y_axis_filters as $key => $filter) {
+                        if ($filter != '0') {
+                            print '<button id="' . $key . '" class="mainFilter btn btn-primary btn-reports">' . $filter . '</button>';
+                        }
+                    }
+                ?>
+            </div>
 
-    <div class="col-md-2 column1 panel-panel">
-        <?php
-            foreach ($enabled_y_axis_filters as $key => $filter) {
-                if ($filter != '0') {
-                    print '<button id="' . $key . '" class="mainFilter btn btn-primary btn-reports">' . $filter . '</button>';
-                }
-            }
-        ?>
+            <div class="col-md-8 column2 panel-panel">
+                <input type="text" id="reportToDate">
+                <div id="custom-report"></div>
+            </div>
+
+            <!-- Content generated in reports.js -->
+            <div class="col-md-8 column1 panel-panel report-x-filters"></div>
+        </div>
     </div>
-
-    <div class="col-md-8 column2 panel-panel">
-
-        <input type="text" id="reportToDate">
-
-        <div id="custom-report"></div>
-    </div>
-
-    <!-- Content generated in reports.js -->
-    <div class="col-md-8 column1 panel-panel report-x-filters"></div>
-
-</div>
+</section>
 
 <div class="panel-pane custom-data-block">
-
     <div>
         <div id="custom-report-details"> <?php print $custom_data; ?> </div>
     </div>
-
 </div>
 
-<div class="panel-pane pane-block custom-settings-block">
-
-    <div class="col-md-8 column2 panel-panel">
+<section class="chr_section">
+    <div class="chr_section__body">
         <?php print $settings_url; ?>
     </div>
-
-</div>
+</section>
