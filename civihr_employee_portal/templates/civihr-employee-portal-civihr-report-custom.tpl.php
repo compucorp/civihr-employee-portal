3f53c28f09ca64c65bd3999860483ff50732833a
coldrunKacper <kacper@coldrun.pl>
2016-04-27 17:40:19 +0200

    develop-reports - Reports code bundle with upgraded Features.

--- civihr_employee_portal/templates/civihr-employee-portal-civihr-report-custom.tpl.php
+++ civihr_employee_portal/templates/civihr-employee-portal-civihr-report-custom.tpl.php
@@ -1,30 +1,3 @@
-<link href="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/pivot.css" rel="stylesheet" />
-<link href="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/c3.min.css" rel="stylesheet" />
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivot.min.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/c3.min.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/d3.min.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/c3_renderers.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable/export_renderers.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable-nreco/jquery-ui-1.9.2.custom.min.js"></script>
-<script src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable-nreco/nrecopivot.js"></script>
-<?php /*<script src="sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable-nreco/nrecowebpivot.js"></script>
-<script src="sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable-nreco/nrecopivotdataapi.js"></script>
-<script src="sites/all/modules/civihr-custom/civihr_employee_portal/js/pivottable-nreco/nrecopivotdataapi.jquery.nrecorelexbuilder-1.0"></script>
- */?>
-
-<?php /******** ORB includes **********/ ?>
-<?php /*<link rel="stylesheet" type="text/css" href="sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/css/bootstrap_superhero.css" />*/ ?>
-<link rel="stylesheet" type="text/css" href="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/css/prism.css" />
-<link rel="stylesheet" type="text/css" href="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/css/main.css" />
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/lib/respond.min.js"></script>
-<link rel="stylesheet" type="text/css" href="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/css/orb.min.css" />
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/lib/react-0.12.2.min.js"></script>
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/orb.min.js"></script>
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/data.js"></script>
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/main.js"></script>
-<script type="text/javascript" src="/sites/all/modules/civihr-custom/civihr_employee_portal/js/orb/prism.js"></script>
-
 <?php if (!empty($filters)): ?>
 <a id="expose-filters-btn" class="btn btn-primary btn-default">Expose filters &raquo;</a>
 <a id="collapse-filters-btn" class="btn btn-primary btn-default hidden">Collapse filters &laquo;</a>
@@ -46,6 +19,7 @@
 </ul>
 
 <div class="report-content">
+<?php if (!empty($tableUrl)): ?>
     <div class="report-block data">
         <h4>Data</h4>
         <div id="reportTable"><?php print $table; ?></div>
@@ -53,6 +27,8 @@
         <a href="<?php print $exportUrl; ?>" id="export-report" class="btn btn-primary btn-default">Export</a>
 <?php endif; ?>
     </div>
+<?php endif; ?>
+<?php if (!empty($jsonUrl)): ?>
     <div class="report-block pivot-table hidden">
         <h4>Pivot Table</h4>
         <div id="reportPivotTable"></div>
@@ -61,183 +37,51 @@
         <h4>Orb Pivot Table (with subtotals)</h4>
         <div id="reportOrbPivotTable"></div>
     </div>
+<?php endif; ?>
 </div>
 
 <script type="text/javascript">
     CRM.$(function () {
 <?php if (!empty($data)): ?>
         var data = <?php print $data; ?>;
-        
-        /*** PivotTable library initialization: ***/
-        jQuery("#reportPivotTable").pivotUI(data, {
-            rendererName: "Table",
-            renderers: CRM.$.extend(
-                jQuery.pivotUtilities.renderers, 
-                jQuery.pivotUtilities.c3_renderers,
-                jQuery.pivotUtilities.export_renderers
-            ),
-            vals: ["Total"],
-            rows: [],
-            cols: [],
-            aggregatorName: "Count",
-            unusedAttrsVertical: false,
-            derivedAttributes: {}
-        }, false);
-        
-        ///// Orb.js:
-        function orbConvertData(data) {
-            var orbData = [];
-            for (var i in data) {
-                var orbRow = [];
-                for (var j in data[i]) {
-                    if (
-                        j === 'Contact ID' || 
-                        j === 'Employee age'
-                    ) {
-                        data[i][j] = parseFloat(data[i][j]);
-                    }
-                    orbRow.push(data[i][j]);
+        var initialDerivedAttributes = {};
+<?php if ($report_name === 'people'): ?>
+        initialDerivedAttributes = {
+            "Employee length of service group": function(row) {
+                var los = parseInt(row['Employee length of service'] / 365, 10);
+                if (los < 1) {
+                    return "Under 1 year";
                 }
-                orbData.push(orbRow);
-            }
-            var orbFields = [];
-            var j = 0;
-            for (var i in data[0]) {
-                orbFields.push({
-                    name: j++,
-                    caption: i
-                });
-            }
-            orbFields.push(
-                {
-                    name: j++,
-                    caption: 'Duration',
-                    dataSettings: {
-                        aggregateFunc: 'avg',
-                        formatFunc: function(value) {
-                            return Number(value).toFixed(0);
-                        }
-                    }
+                if (los < 2) {
+                    return "1 - 2 years";
                 }
-            );
-    
-            return {
-                'data': orbData,
-                'fields': orbFields
+                if (los < 5) {
+                    return "2 - 5 years";
+                }
+                if (los < 10) {
+                    return "5 - 10 years";
+                }
+                if (los < 15) {
+                    return "10 - 15 years";
+                }
+                if (los < 20) {
+                    return "15 - 20 years";
+                }
+                return "Over 20 years";
             }
         }
-        
-        var orbConfig = function(f, d) {
-            return {
-                width: 1110,
-                height: 645,
-                dataSource: d,
-                dataHeadersLocation: 'columns',
-                theme: 'blue',
-                toolbar: {
-                    visible: true
-                },
-                grandTotal: {
-                        rowsvisible: true,
-                        columnsvisible: true
-                },
-                subTotal: {
-                        visible: true,
-                    collapsed: true
-                },
-                fields: f,
-                rows    : [],
-                columns : [],
-                data    : [],
-                preFilters : {
-                }
-            };
-        };
-        var orbElem = document.getElementById('reportOrbPivotTable');
-        var orbConvertedData = orbConvertData(data);
-        var orbInstance = new orb.pgridwidget(orbConfig(orbConvertedData.fields, orbConvertedData.data));
-        orbInstance.render(orbElem);
 <?php endif; ?>
-        ////////////////////////////////////////////////////////////////////////
-
-<?php if (!empty($jsonUrl)): ?>
-        function reportRefreshJson(filterValues) {
-            console.info('reportRefreshJson()');
-            CRM.$.ajax({
-                url: '<?php print $jsonUrl; ?>' + filterValues,
-                error: function () {
-                    console.info('error');
-                },
-                success: function (data) {
-                    // Refreshing Pivot Table:
-                    console.info('refreshing Pivot Table');
-                    jQuery("#reportPivotTable").pivotUI(data, {
-                        rendererName: "Table",
-                        renderers: CRM.$.extend(
-                            jQuery.pivotUtilities.renderers, 
-                            jQuery.pivotUtilities.c3_renderers
-                        ),
-                        unusedAttrsVertical: false
-                    }, false);
-                    // Refreshing Orb Pivot Table:
-                    var orbConvertedData = orbConvertData(data);
-                    orbInstance.refreshData(orbConvertedData.data);
-                },
-                type: 'GET'
-            });
-        }
-<?php endif; ?>
-<?php if (!empty($tableUrl)): ?>
-        function reportRefreshTable(filterValues) {
-            console.info('reportRefreshTable()');
-            CRM.$.ajax({
-                url: '<?php print $tableUrl; ?>' + filterValues,
-                error: function () {
-                    console.info('error');
-                },
-                success: function (data) {
-                    CRM.$('#reportTable').html(data);
-                    //CRM.$('#reportTable form').hide(); // temporary
-                },
-                type: 'GET'
-            });
-        }
-        //CRM.$('#reportTable form').hide(); // temporary
-<?php endif; ?>
-<?php if (!empty($filters)): ?>
-        CRM.$('#expose-filters-btn').bind('click', function(e) {
-            e.preventDefault();
-            CRM.$(this).addClass('hidden');
-            CRM.$('#collapse-filters-btn').removeClass('hidden');
-            CRM.$('#report-filters').removeClass('hidden');
+        Drupal.behaviors.civihr_employee_portal_reports.instance.init({
+            data: data,
+            tableContainer: jQuery('#reportTable'),
+            pivotTableContainer: jQuery('#reportPivotTable'),
+            orbContainer: document.getElementById('reportOrbPivotTable'),
+            derivedAttributes: initialDerivedAttributes,
+            tableUrl: '<?php print $tableUrl; ?>',
+            jsonUrl: '<?php print $jsonUrl; ?>',
+            filters: <?php print !empty($filters) ? 1 : 0; ?>
         });
-        CRM.$('#collapse-filters-btn').bind('click', function(e) {
-            e.preventDefault();
-            CRM.$(this).addClass('hidden');
-            CRM.$('#expose-filters-btn').removeClass('hidden');
-            CRM.$('#report-filters').addClass('hidden');
-        });
-        CRM.$('#report-filters input[type="submit"]').bind('click', function(e) {
-            e.preventDefault();
-            console.info('submitting filters');
-            var formSerialize = CRM.$('#report-filters form:first').formSerialize();
-            console.info('formSerialize:');
-            console.info(formSerialize);
-<?php if (!empty($jsonUrl)): ?>
-            reportRefreshJson('?' + formSerialize);
+        Drupal.behaviors.civihr_employee_portal_reports.instance.show();
 <?php endif; ?>
-<?php if (!empty($tableUrl)): ?>
-            reportRefreshTable('?' + formSerialize);
-<?php endif; ?>
-        });
-<?php endif; ?>
-        CRM.$('.report-tabs a').bind('click', function(e) {
-            e.preventDefault();
-            CRM.$('.report-tabs li').removeClass('active');
-            CRM.$(this).parent().addClass('active');
-            CRM.$('.report-block').addClass('hidden');
-            CRM.$('.report-block.' + CRM.$(this).data('tab')).removeClass('hidden');
-        });
-        CRM.$('.report-tabs a:first').click();
     });
 </script>
