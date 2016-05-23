2e4b0cb7a0d7a97164ebec9b706b40ede17fb79e
Bruno Queiros <brunoofq@gmail.com>
2016-05-23 08:29:26 -0300

    PCHR-1004: Add panel classes on reports list and create a method to set the title markup

--- civihr_employee_portal/templates/civihr-employee-portal-civihr-report-landing-page.tpl.php
+++ civihr_employee_portal/templates/civihr-employee-portal-civihr-report-landing-page.tpl.php
@@ -1,12 +1,30 @@
 <ul>
 <?php
+
+/**
+ * Split the title on ':' to separate the subtitle
+ * and add the class .chr_panel__header__subtitle to it.
+ *
+ * @param string $originalTitle
+ * @return string
+ */
+function setTitleMarkup($originalTitle) {
+  $position = strpos($originalTitle, ':') + 1;
+  $title = substr($originalTitle, 0, $position);
+  $subtitle = '<span class="chr_panel__header__subtitle">' . substr($originalTitle, $position) . '</span>';
+
+  return $title . $subtitle;
+}
+
 foreach ($menu as $item):
 ?>
-    <li>
+    <li class="panel-pane pane-block chr_panel">
         <a href="<?php print $item['link_path']; ?>">
-            <h2><?php print $item['link_title']; ?></h2>
-            <?php print $item['options']['attributes']['title']; ?>
+            <h2 class="chr_panel__header chr_panel__header--with-border panel-title"><?php print setTitleMarkup($item['link_title']); ?></h2>
         </a>
+        <div>
+            <?php print $item['options']['attributes']['title']; ?>
+        </div>
     </li>
 <?php
 endforeach;
