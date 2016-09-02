91b40f385d6b079c3d24b4390e0c017d327b3d4f
coldrunKacper <kacper@coldrun.pl>
2016-09-02 11:54:00 +0200

    PCHR-1540 - Adding custom callback for getting Reports JSON data.

--- civihr_employee_portal/templates/civihr-employee-portal-civihr-report-custom.tpl.php
+++ civihr_employee_portal/templates/civihr-employee-portal-civihr-report-custom.tpl.php
@@ -63,7 +63,6 @@
 
   <script type="text/javascript">
       CRM.$(function () {
-  <?php if (!empty($data)): ?>
           var data = <?php print $data; ?>;
           var initialDerivedAttributes = {};
   <?php if ($report_name === 'people'): ?>
@@ -102,7 +101,6 @@
               filters: <?php print !empty($filters) ? 1 : 0; ?>
           });
           Drupal.behaviors.civihr_employee_portal_reports.instance.show();
-  <?php endif; ?>
       });
   </script>
 </div>
