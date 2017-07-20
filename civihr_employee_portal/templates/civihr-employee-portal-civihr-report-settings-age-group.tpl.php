081303ced4880315908bfac969f217f6a0e66e59
Debarshi Bhaumik <deb1990@gmail.com>
2017-07-20 20:14:04 +0530

    PCHR-2417: PR Feedback

--- civihr_employee_portal/templates/civihr-employee-portal-civihr-report-settings-age-group.tpl.php
+++ civihr_employee_portal/templates/civihr-employee-portal-civihr-report-settings-age-group.tpl.php
@@ -6,6 +6,7 @@
         <span class="chr_search-result__total__count"><?php echo count($rows); ?></span>
       </div>
     </div>
+    <!--Adding "table" class to get the bootstrap styles-->
     <?php $classes .= ' table '; ?>
     <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
       <?php if (!empty($title) || !empty($caption)) : ?>
