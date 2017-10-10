90c021c9b4ca71647ec949d1b4c5582a02dd7838
Buddha Man Nepali <bmnepali980@gmail.com>
2017-10-10 15:46:36 +0545

    PCHR-2457: Fix semistandard style guide issues

--- civihr_employee_portal/js/ta-documents-app.js
+++ civihr_employee_portal/js/ta-documents-app.js
@@ -1,6 +1,6 @@
 /* globals angular */
 
-(function(angular) {
+(function (angular) {
   angular.module('taDocuments', ['civitasks.appDocuments', 'civitasks.directives'])
     .config(function ($urlRouterProvider, $locationProvider) {
       $locationProvider.html5Mode(true); // This is required to remove # for the URL
@@ -8,104 +8,103 @@
     })
     .controller('ModalController', ModalController);
 
-    ModalController.$inject = ['$scope', '$rootScope', '$window', '$rootElement', '$log', '$uibModal',
-      'DocumentService', 'FileService', 'config', 'settings', 'DateFormat'];
+  ModalController.$inject = ['$scope', '$rootScope', '$window', '$rootElement', '$log', '$uibModal',
+    'DocumentService', 'FileService', 'config', 'settings', 'DateFormat'];
 
-    function ModalController ($scope, $rootScope, $window, $rootElement, $log, $modal, DocumentService, FileService, config,
-      settings, DateFormat) {
-        var availableContacts = false;
-        var vm = this;
+  function ModalController ($scope, $rootScope, $window, $rootElement, $log, $modal, DocumentService, FileService, config,
+    settings, DateFormat) {
+    var vm = this;
 
-        vm.modalDocument = modalDocument;
+    vm.modalDocument = modalDocument;
 
-        (function init() {
-          // Sets the date format for HR_settings.DATE_FORMAT
-          DateFormat.getDateFormat();
-          subscribeForEvents();
-        })();
+    (function init () {
+      // Sets the date format for HR_settings.DATE_FORMAT
+      DateFormat.getDateFormat();
+      subscribeForEvents();
+    })();
 
-        /**
-         * Collect required contact and cache them for document modal
-         *
-         * @return {promise}
-         */
-        function cacheContacts (documents) {
-          return DocumentService.cacheContactsAndAssignments(documents, 'contacts');
-        };
-
-        /**
-         * Gets Document for the given document id and
-         * opens Modal with retrived document data
-         *
-         * @param {integer} id
-         * @param {string} role
-         * @param {string} mode
-         */
-        function modalDocument (id, role, mode) {
-          DocumentService.get({ id: id })
-            .then(function (data) {
-              return {
-                data: data,
-                isOpened: openModalDocument(data[0], role, mode)
-              };
-            })
-            .then(function (result) {
-              return cacheContacts(result.data);
-            })
-            .then(function () {
-              $rootScope.isLoading = false;
-            })
-            .catch(function (reason) {
-              CRM.alert(reason, 'Error', 'error');
-            });
-        };
-
-        /**
-         * Opens Document Modal
-         *
-         * @param {object} data
-         * @param {string} role
-         * @param {string} mode
-         * @param {promise}
-         */
-        function openModalDocument (data, role, mode) {
-          var modalInstance = $modal.open({
-            appendTo: $rootElement,
-            templateUrl: config.path.TPL + 'modal/document.html?v=3',
-            controller: 'ModalDocumentController',
-            controllerAs: 'documentModal',
-            resolve: {
-              modalMode: function () {
-                return mode;
-              },
-              role: function () {
-                return role;
-              },
-              data: function () {
-                return data;
-              },
-              files: function () {
-                if (!data.id || !+data.file_count) {
-                  return [];
-                }
-
-                return FileService.get(data.id, 'civicrm_activity');
-              }
-            }
-          });
-
-          return modalInstance.opened.then(function () {
-            $rootScope.isLoading = true;
-          });
-        }
-
-        /**
-         * All event subscribers
-         */
-        function subscribeForEvents () {
-          $rootScope.$on('document-saved', function () {
-            $window.location.reload();
-          });
-        };
+    /**
+     * Collect required contact and cache them for document modal
+     *
+     * @return {promise}
+     */
+    function cacheContacts (documents) {
+      return DocumentService.cacheContactsAndAssignments(documents, 'contacts');
     }
+
+    /**
+     * Gets Document for the given document id and
+     * opens Modal with retrived document data
+     *
+     * @param {integer} id
+     * @param {string} role
+     * @param {string} mode
+     */
+    function modalDocument (id, role, mode) {
+      DocumentService.get({ id: id })
+        .then(function (data) {
+          return {
+            data: data,
+            isOpened: openModalDocument(data[0], role, mode)
+          };
+        })
+        .then(function (result) {
+          return cacheContacts(result.data);
+        })
+        .then(function () {
+          $rootScope.isLoading = false;
+        })
+        .catch(function (reason) {
+          CRM.alert(reason, 'Error', 'error');
+        });
+    }
+
+    /**
+     * Opens Document Modal
+     *
+     * @param {object} data
+     * @param {string} role
+     * @param {string} mode
+     * @param {promise}
+     */
+    function openModalDocument (data, role, mode) {
+      var modalInstance = $modal.open({
+        appendTo: $rootElement,
+        templateUrl: config.path.TPL + 'modal/document.html?v=3',
+        controller: 'ModalDocumentController',
+        controllerAs: 'documentModal',
+        resolve: {
+          modalMode: function () {
+            return mode;
+          },
+          role: function () {
+            return role;
+          },
+          data: function () {
+            return data;
+          },
+          files: function () {
+            if (!data.id || !+data.file_count) {
+              return [];
+            }
+
+            return FileService.get(data.id, 'civicrm_activity');
+          }
+        }
+      });
+
+      return modalInstance.opened.then(function () {
+        $rootScope.isLoading = true;
+      });
+    }
+
+   /**
+    * All event subscribers
+    */
+    function subscribeForEvents () {
+      $rootScope.$on('document-saved', function () {
+        $window.location.reload();
+      });
+    }
+  }
 })(angular);
