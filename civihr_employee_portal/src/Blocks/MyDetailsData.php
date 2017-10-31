b88ddb1462831ffe8aea8aab0f5d8fa1833754d6
Michael Devery <michael@compucorp.co.uk>
2017-10-31 16:34:43 +0000

    PCHR-2848: Remove conditional to change the view on the hr-details page

--- civihr_employee_portal/src/Blocks/MyDetailsData.php
+++ civihr_employee_portal/src/Blocks/MyDetailsData.php
@@ -14,29 +14,13 @@ class MyDetailsData {
 
         }
 
-        // @TODO -> show different view with all the data available
-        // If we are on the HR details full list page, show different view with all the data
-        if (isset($_GET['q']) && $_GET['q'] == 'hr-details') {
+        // Get the contact details view
+        $contact_details = views_embed_view('my_details_block', 'my_details_block');
 
-            // Get the contact details view
-            $contact_details = views_embed_view('my_details_block', 'my_details_block_full');
+        // Get the address details view
+        $address_data = views_embed_view('my_details_block', 'my_address_block');
+        $address_data_title = t('Contact Information');
 
-            // Get the emergency contacts block (not the address data)
-            $address_data = views_embed_view('my_details_block', 'my_details_emergency_contact');
-            $address_data_title = t('Emergency Contact');
-
-        }
-        else {
-
-            // Get the contact details view
-            $contact_details = views_embed_view('my_details_block', 'my_details_block');
-
-            // Get the address details view
-            $address_data = views_embed_view('my_details_block', 'my_address_block');
-            $address_data_title = t('Contact Information');
-
-        }
-        
         // Output the themed details block
         return theme('civihr_employee_portal_my_details_block', 
             array(
