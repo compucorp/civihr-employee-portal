160a268034715abed0e35e6c37df2cae53fcf164
Michael Devery <michael@compucorp.co.uk>
2018-02-08 12:53:13 +0000

    PCHR-3269: Add Url Helper class and tests

---
+++ civihr_employee_portal/src/Helpers/UrlHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\civihr_employee_portal\Helpers;
+
+class UrlHelper {
+
+  /**
+   * Removes duplicate query parts from a url. Accepts the value for the first
+   * part.
+   *
+   * @param string $url
+   *
+   * @return string
+   */
+  public static function dedupeUrlQueryParams($url) {
+    $origQueryString = parse_url($url, PHP_URL_QUERY);
+    $queryParts = explode('&', $origQueryString);
+
+    $queryPartsUnique = [];
+    foreach ($queryParts as $queryPart) {
+      list($key) = explode('=', $queryPart);
+      if (!isset($queryPartsUnique[$key])) {
+        $queryPartsUnique[$key] = $queryPart;
+      }
+    }
+
+    $newQueryString = implode('&', $queryPartsUnique);
+
+    return str_replace($origQueryString, $newQueryString, $url);
+  }
+
+}
---
+++ civihr_employee_portal/tests/Helpers/UrlHelperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Drupal\civihr_employee_portal\Helpers\UrlHelper;
+
+class UrlHelperTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * @dataProvider duplicateQueryUrlProvider
+   *
+   * @param string $url
+   * @param string $expected
+   */
+  public function testDedupeWillReturnExpectedResults($url, $expected) {
+    $this->assertEquals($expected, UrlHelper::dedupeUrlQueryParams($url));
+  }
+
+  /**
+   * @return array
+   */
+  public function duplicateQueryUrlProvider() {
+    return [
+      [
+        '',
+        '',
+      ],
+      [
+        'http://www.civihr.net',
+        'http://www.civihr.net',
+      ],
+      [
+        'http://civihr.net/sites/default/files/webform/500_3.jpg?photo=0&photo=0&photo=0&photo=0&photo=0&photo=0',
+        'http://civihr.net/sites/default/files/webform/500_3.jpg?photo=0',
+      ],
+      [
+        'http://civihr.local/civicrm/contact/imagefile?photo=pp_2e6adadea8d41b0ac065e888f2e61dc5.jpeg',
+        'http://civihr.local/civicrm/contact/imagefile?photo=pp_2e6adadea8d41b0ac065e888f2e61dc5.jpeg',
+      ],
+      [
+        'http://civihr.local/civicrm/contact/imagefile?photo=pp_2e6adadea8d41b0ac065e888f2e61dc5.jpeg&photo=0',
+        'http://civihr.local/civicrm/contact/imagefile?photo=pp_2e6adadea8d41b0ac065e888f2e61dc5.jpeg',
+      ],
+      [
+        'http://www.civihr.net?foo=bar&bar=car&foo=bar2&bar=car2#someanchor',
+        'http://www.civihr.net?foo=bar&bar=car#someanchor',
+      ],
+    ];
+  }
+
+}
