4bcb08e17753072f32276c4cbe64a9a5e87c0147
Debarshi Bhaumik <deb1990@gmail.com>
2018-02-22 13:59:55 +0530

    PCHR-3342: Style Users List

--- civihr_employee_portal/templates/views-view-table.tpl.php
+++ civihr_employee_portal/templates/views-view-table.tpl.php
@@ -1,6 +1,8 @@
 <?php
 if (_is_hrreports_data_table_view($view->name)) {
-    include('views-view-table-hrreports.tpl.php');
+  include('views-view-table-hrreports.tpl.php');
+} else if (_is_scrollable_table_view_with_results($view->name)) {
+  include('views-view-table-scrollable-with-results.tpl.php');
 } else {
-    include('views-view-table-default.tpl.php');
+  include('views-view-table-default.tpl.php');
 }
