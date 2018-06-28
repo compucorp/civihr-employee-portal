f11df1d92887a20266dd2f71b862c41e57348f21
Beto Aveiga <luis.aveiga@gmail.com>
2018-06-27 22:14:35 -0500

    PCHR-3817: Add custom templates for views in My Details page

---
+++ civihr_employee_portal/views/templates/views-view--my-details.tpl.php
@@ -0,0 +1,7 @@
+<?php
+  $rows .= $footer;
+  $footer = '
+    <a href="/edit-my-contact-details/js/view" class="ctools-use-modal ctools-modal-civihr-custom-style  btn btn-primary">
+      <i class="fa fa-pencil" aria-hidden="true"></i> Edit Contact Details
+    </a>';
+  include('views-view--generic-display-output.tpl.php');
---
+++ civihr_employee_portal/views/templates/views-view-unformatted--mydetails-myrole.tpl.php
@@ -0,0 +1,4 @@
+<?php
+  foreach ($rows as $id => $row) {
+    print $row;
+  }
