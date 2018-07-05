123a0b48163b417781f777cc5910c90e3cce9fff
Beto Aveiga <luis.aveiga@gmail.com>
2018-07-04 19:31:24 -0500

    PCHR-3817: document vars in _views-view--generic-display-output.tpl.php

--- civihr_employee_portal/views/templates/_views-view--generic-display-output.tpl.php
+++ civihr_employee_portal/views/templates/_views-view--generic-display-output.tpl.php
@@ -5,24 +5,24 @@
  * Main view template.
  *
  * Variables available:
- * - $classes_array: An array of classes determined in
+ * - @var array $classes_array: An array of classes determined in
  *   template_preprocess_views_view(). Default classes are:
  *     .view
  *     .view-[css_name]
  *     .view-id-[view_name]
  *     .view-display-id-[display_name]
  *     .view-dom-id-[dom_id]
- * - $classes: A string version of $classes_array for use in the class attribute
- * - $css_name: A css-safe version of the view name.
- * - $css_class: The user-specified classes names, if any
- * - $header: The view header
- * - $footer: The view footer
- * - $rows: The results of the view query, if any
- * - $empty: The empty text to display if the view is empty
- * - $pager: The pager next/prev links to display, if any
- * - $exposed: Exposed widget form/info to display
- * - $feed_icon: Feed icon to display, if any
- * - $more: A link to view more, if any
+ * - @var string $classes: A string version of $classes_array for use in the class attribute
+ * - @var string $css_name: A css-safe version of the view name.
+ * - @var string $css_class: The user-specified classes names, if any
+ * - @var string $header: The view header
+ * - @var string $footer: The view footer
+ * - @var string $rows: The results of the view query, if any
+ * - @var string $empty: The empty text to display if the view is empty
+ * - @var string $pager: The pager next/prev links to display, if any
+ * - @var string $exposed: Exposed widget form/info to display
+ * - @var string $feed_icon: Feed icon to display, if any
+ * - @var string $more: A link to view more, if any
  *
  * @ingroup views_templates
  */
