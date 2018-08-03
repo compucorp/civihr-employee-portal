1bf092050ac0af0bada0167603b2197c4ec721c3
Beto Aveiga <luis.aveiga@gmail.com>
2018-08-02 23:26:25 -0500

    PCHR-3928: refactor AbstractDrupalSystemMail class and classes that use it

--- civihr_employee_portal/src/Mail/AbstractDrupalSystemMail.php
+++ civihr_employee_portal/src/Mail/AbstractDrupalSystemMail.php
@@ -4,27 +4,113 @@ namespace Drupal\civihr_employee_portal\Mail;
 
 abstract class AbstractDrupalSystemMail {
   /**
+   * Returns the name of the template for this mail
+   *
    * @return string
+   *     the name of the template
    */
-  abstract public function getTemplateName();
+  protected abstract function getTemplateName();
 
   /**
-   * @param array $message
+   * Set variables that will be passed to the template
    *
-   * @return array
+   * @return AbstractDrupalSystemMail
+   *     this, an instance of the the same Class
    */
-  public function getVariables($message) {
-    return [];
+  protected abstract function buildVariables();
+
+  /**
+   * Template system to build notifications
+   *
+   * @var \CRM_Core_Smarty
+   */
+  static protected $smarty;
+
+  /**
+   * Message variable passed by reference that will be used to send the email by Drupal
+   * @var array
+   */
+  protected $message;
+
+  /**
+   * Multiple parameters that will be used during the build of a notification
+   *
+   * @var array
+   */
+  protected $params;
+
+  /**
+   * Structured data that is build to be passed to notification templates
+   *
+   * @var array
+   */
+  protected $variables;
+
+  /**
+   * Initializes properties
+   *
+   * @param array $message
+   *    the $message variable from a mail function passed by reference
+   *
+   * @param array $params
+   *    initial parameters to build notifications
+   */
+  public function __construct(&$message = null, $params = []) {
+    $this->message = &$message;
+    $this->params = $params;
+
+    $message['module'] = 'civihr_employee_portal';
+    if (isset($params['key'])) {
+      $message['key'] = $params['key'];
+    }
+    if (isset($params['subject'])) {
+      $message['subject'] = $params['subject'];
+    }
+  }
+
+  public static function init($templatePath) {
+    self::$smarty = \CRM_Core_Smarty::singleton();
+    self::$smarty->addTemplateDir($templatePath);
   }
 
   /**
-   * @return array
+   * Creates headers and body for a notification
+   *
+   * @return AbstractDrupalSystemMail
+   *     this, the same instance
    */
-  public function getHeaders() {
-    return $headers = [
+  public function processMessage() {
+    $this->buildVariables()->buildHeaders()->buildBody();
+    return $this;
+  }
+
+  /**
+   * Build body for a notification
+   *
+  * @return AbstractDrupalSystemMail
+   *     this, the same instance
+   */
+  protected function buildBody() {
+    $this->message['body'] = [self::$smarty->fetchWith($this->getTemplateName(), $this->variables)];
+    return $this;
+  }
+
+  /**
+   * Build headers for a notification
+   *
+   * @return AbstractDrupalSystemMail
+   *     this, the same instance
+   */
+  protected function buildHeaders() {
+    $headers = [
       'MIME-Version' => '1.0',
       'Content-Type' => 'text/html; charset=UTF-8;'
     ];
+    $this->message['headers'] = array_merge($this->message['headers'], $headers);
+    return $this;
   }
-
 }
+
+AbstractDrupalSystemMail::init(
+  DRUPAL_ROOT . '/' . drupal_get_path('module', 'civihr_employee_portal') . '/templates/smarty'
+);
