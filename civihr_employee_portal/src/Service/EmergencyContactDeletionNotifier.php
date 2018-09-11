5f150f95737c82cadbe47af03a6644e9a87f2655
Michael Devery <michael@compucorp.co.uk>
2018-09-11 15:31:23 +0100

    PCHR-4175: Rename deletion service, make endpoint return JSON

---
+++ civihr_employee_portal/src/Service/EmergencyContactDeletionNotifier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\civihr_employee_portal\Service;
+
+use CRM_Hremergency_Service_EmergencyContactService as EmergencyContactService;
+
+class EmergencyContactDeletionNotifier {
+
+  /**
+   * Sends the notification mail about emergency contact deletion
+   *
+   * @param int $emergencyContactId
+   */
+  public function notifyDeletion($emergencyContactId) {
+    if (!WebformSubmissionSettingsService::shouldSendMail()) {
+      return;
+    }
+
+    $service = new EmergencyContactService();
+    $emergencyContact = $service->find($emergencyContactId);
+
+    $params = ['emergencyContact' => $emergencyContact];
+    $mail = WebformSubmissionSettingsService::getTargetEmail();
+    $module = 'civihr_employee_portal';
+    $lang = language_default();
+    drupal_mail($module, 'emergency_contact_deletion', $mail, $lang, $params);
+  }
+
+}
