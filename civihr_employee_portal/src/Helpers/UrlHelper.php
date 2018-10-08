f3d645ad9588f1159f2a325e566e59780fe33721
Michael Devery <michael@compucorp.co.uk>
2018-10-08 16:22:55 +0100

    PCHR-4281: Add function for removing single query value from URL

--- civihr_employee_portal/src/Helpers/UrlHelper.php
+++ civihr_employee_portal/src/Helpers/UrlHelper.php
@@ -38,4 +38,44 @@ class UrlHelper {
     return str_replace($origQueryString, $newQueryString, $url);
   }
 
+  /**
+   * Removes the provided key from the URL query parameters
+   *
+   * @param string $url
+   *   The URL to edit, e.g. http://example.com?foo=bar&bar=zoo
+   * @param string $targetKey
+   *   The key you want to remove e.g. bar
+   *
+   * @return string
+   *   Original URL without the target query, e.g. http://example.com?foo=bar
+   */
+  public static function removeQueryValueFromUrl($url, $targetKey) {
+    $origQueryString = parse_url($url, PHP_URL_QUERY);
+    $queryParts = explode('&', $origQueryString);
+
+    $newQueryParts = [];
+    foreach ($queryParts as $index => $queryPart) {
+      list($key) = explode('=', $queryPart);
+
+      $keyToCompare = $targetKey;
+      if (substr($key, -2) === '[]') {
+        $keyToCompare = $targetKey . '[]';
+      }
+
+      if ($key !== $keyToCompare) {
+        $newQueryParts[$key] = $queryPart;
+      }
+    }
+
+    $newQueryString = implode('&', $newQueryParts);
+    $newUrl = str_replace($origQueryString, $newQueryString, $url);
+
+    if (!$newQueryString) {
+      // remove ? if no query string remains
+      $newUrl = str_replace('?', '', $newUrl);
+    }
+
+    return $newUrl;
+  }
+
 }
