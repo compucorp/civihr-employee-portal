64a84cfd77351314fd5cbdf1e2c38c4b0ca20586
Samson Alajede <ajesamson@yahoo.com>
2018-12-28 13:14:37 +0100

    PCHR-4405: Refactored to maintain existing default personal address and enforce only one personal address

--- civihr_employee_portal/updates/7044.php
+++ civihr_employee_portal/updates/7044.php
@@ -12,26 +12,26 @@ function civihr_employee_portal_update_7044() {
 
   $addresses = _get_personal_address_count();
   foreach ($addresses as $address) {
-    if ($address['total'] < 2) {
+    if ($address['total'] != 2) {
       continue;
     }
 
     $result = civicrm_api3('Address', 'get', [
       'location_type_id' => 'Personal',
-      'contact_id' => $address['contact_id'],
+      'contact_id' => $address['contact_id']
     ]);
-    $updated = 0;
-    foreach ($result['values'] as $index => $contactAddress) {
-      if ($updated === 0) {
-        $contactAddress['is_primary'] = 1;
+    $processed = 0;
+    foreach ($result['values'] as $contactAddress) {
+      $updateAnyAddress = $contactAddress['is_primary'] == 0 && $address['primary_count'] == 1;
+      $updateSecondAddress = $address['primary_count'] != 1 && $processed == 1;
+
+      if ($updateAnyAddress || $updateSecondAddress) {
+        $contactAddress['location_type_id'] = $locationType['id'];
+        $contactAddress['is_primary'] = 0;
         civicrm_api3('Address', 'create', $contactAddress);
-        $updated++;
-        continue;
       }
 
-      $contactAddress['is_primary'] = 0;
-      $contactAddress['location_type_id'] = $locationType['id'];
-      civicrm_api3('Address', 'create', $contactAddress);
+      $processed++;
     }
   }
 }
@@ -43,7 +43,8 @@ function civihr_employee_portal_update_7044() {
  */
 function _get_personal_address_count() {
   $query = "
-    SELECT a.contact_id, COUNT(a.location_type_id) AS total FROM civicrm_address a
+    SELECT a.contact_id, COUNT(a.location_type_id) AS total, SUM(a.is_primary) AS primary_count
+    FROM civicrm_address a
     INNER JOIN civicrm_location_type lt ON (a.location_type_id = lt.id)
     WHERE lt.name = 'Personal'
     GROUP BY a.contact_id
